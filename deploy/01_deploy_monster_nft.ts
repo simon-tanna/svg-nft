5d079a8236fc9bf595400fc61d206642528a33e6
simon-tanna <gcab022109@coderacademy.edu.au>
2022-08-24 12:34:13 +1000

    created helper config file for networks

--- deploy/01_deploy_monster_nft.ts
+++ deploy/01_deploy_monster_nft.ts
@@ -1,5 +1,8 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import * as fs from "fs";
+import { ethers } from "hardhat";
+import { networkConfig } from "../helper-hardhat-config";
 
 const deployMonsterNFT: DeployFunction = async function (
 	hre: HardhatRuntimeEnvironment
@@ -17,6 +20,22 @@ const deployMonsterNFT: DeployFunction = async function (
 		log: true,
 	});
 	log(`You have deployed an NFT contract to ${MonsterNFT.address}`);
+	// read the svg
+	let filepath = "./img/patternpad.svg";
+	let svg = fs.readFileSync(filepath, { encoding: "utf8" });
+	// svg will be passed in to the create function of the contract
+	const monsterNFTContract = await ethers.getContractFactory("MonsterNFT");
+	const accounts = await hre.ethers.getSigners();
+	const signer: any = accounts[0];
+	const monsterNFT = new ethers.Contract(
+		MonsterNFT.address,
+		monsterNFTContract.interface,
+		signer
+	);
+	const networkName = networkConfig[chainId]["name"];
+	log(
+		`Verify with: \n npx hardhat verify --network ${networkName} ${monsterNFT.address}`
+	);
 };
 
 // module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
