e4ae3f9e29536499f56b52e9dbe68097651d2a2b
simon-tanna <gcab022109@coderacademy.edu.au>
2022-08-24 10:30:00 +1000

    created function to convert svg to base 64 and then return image URI

--- contracts/MonsterNFT.sol
+++ contracts/MonsterNFT.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 // Give the contract SVG code
 // output NFT URI with the SVG code
@@ -24,15 +25,37 @@ contract MonsterNFT is ERC721URIStorage, Ownable {
     }
 
     function create(string memory svg) public {
-        
-    }
-
-    function safeMint() public {
+        // SVG needs to become a token URI (Unique Resource Identifier)
+        // this is the image URI
+        string memory imgURI = svgToImageURI(svg);
         tokenAddress.transferFrom(msg.sender, address(this), tokenRate);
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
     }
+
+    // function to make svg image readable in the create function to mint the NFT
+    function svgToImageURI(string memory svg)
+        public
+        pure
+        returns (string memory)
+    {
+        // using the openzep base64 contract as a util
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgToBase64 = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        // concat the baseURL and svgToBase64
+        string memory imgURI = string(abi.encodePacked(baseURL, svgToBase64));
+        return imgURI;
+    }
+
+    // function safeMint() public {
+    //     tokenAddress.transferFrom(msg.sender, address(this), tokenRate);
+    //     uint256 tokenId = _tokenIdCounter.current();
+    //     _tokenIdCounter.increment();
+    //     _safeMint(msg.sender, tokenId);
+    // }
 
     function withdrawToken() public onlyOwner {
         tokenAddress.transfer(
